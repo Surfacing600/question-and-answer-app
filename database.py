26accce92f92e101eabb25ecc1195250408499e9
Surfacing600 <74630359+Surfacing600@users.noreply.github.com>
2021-01-18 21:11:51 +0000

    Added Procfile

---
+++ database.py
@@ -0,0 +1,14 @@
+from flask import g
+import sqlite3
+
+def connect_db(): #connects to the database
+    sql = sqlite3.connect('/Users/Dmitry/Desktop/question_and_answer_app/questions.db')
+    sql.row_factory = sqlite3.Row #to get dictionaries instead of tuples when you access the rows
+    return sql
+
+
+def get_db():
+    if not hasattr(g, 'sqlite3_db'):#checks if global object 'sqlite3_db' is in the database
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
