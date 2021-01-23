c07e1d44c92d8162022009b21e7f24c62eb9260d
Surfacing600 <74630359+Surfacing600@users.noreply.github.com>
2021-01-23 12:46:28 +0000

    changed code to work with postgress

--- database.py
+++ database.py
@@ -1,14 +1,36 @@
 from flask import g
-import sqlite3
+import psycopg2
+from psycopg2.extras import DictCursor
 
-def connect_db(): #connects to the database
-    sql = sqlite3.connect('/Users/Dmitry/Desktop/question_and_answer_app/questions.db')
-    sql.row_factory = sqlite3.Row #to get dictionaries instead of tuples when you access the rows
-    return sql
-
+def connect_db():
+    conn = psycopg2.connect('postgres://zoozknakxgrwhq:ea45dc1eb54e5c54a094154f775d68a621d0193862893d08c84b7cf1c8ad83b0@ec2-52-72-190-41.compute-1.amazonaws.com:5432/ddashsk37vtpjb', cursor_factory=DictCursor)
+    conn.autocommit = True
+    sql = conn.cursor()
+    return conn, sql
 
 def get_db():
-    if not hasattr(g, 'sqlite3_db'):#checks if global object 'sqlite3_db' is in the database
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+    db = connect_db()
 
+    if not hasattr(g, 'postgres_db_conn'):
+        g.postgres_db_conn = db[0]
+
+    if not hasattr(g, 'postgres_db_cur'):
+        g.postgres_db_cur = db[1]
+
+    return g.postgres_db_cur
+
+def init_db():
+    db = connect_db()
+
+    db[1].execute(open('schema.sql', 'r').read())
+    db[1].close()
+
+    db[0].close()
+
+def init_admin():
+    db = connect_db()
+
+    db[1].execute('update users set admin = True where name = %s', ('admin', ))
+
+    db[1].close()
+    db[0].close()
