766e1c95f06f9ae87e65b420ce4227345d9a8528
Surfacing600 <74630359+Surfacing600@users.noreply.github.com>
2021-01-31 12:36:41 +0000

    test

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import psycopg2
 from psycopg2.extras import DictCursor
 
 def connect_db():
-    conn = psycopg2.connect('postgres://fqfjrrjaoijwlh:facdfedceedf8860b0e5142825eec39eded911180a11faf360a91ed8ce103c38@ec2-54-242-120-138.compute-1.amazonaws.com:5432/d5mbda7cr5bb8o', cursor_factory=DictCursor)
+    conn = psycopg2.connect('postgres://jqfnmdamxelzbx:c33853bb72cc77a3c771743198b2aab7078ff52f937e68d9105a9163036862de@ec2-54-242-120-138.compute-1.amazonaws.com:5432/d19cccc1ggq1or', cursor_factory=DictCursor)
     conn.autocommit = True
     sql = conn.cursor()
     return conn, sql
@@ -30,7 +30,7 @@ def init_db():
 def init_admin():
     db = connect_db()
 
-    db[1].execute('update users set admin = True where name = %s', ('Dima', ))
+    db[1].execute('update users set admin = True where name = %s', ('admin', ))
 
     db[1].close()
     db[0].close()
--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 create table users (
     id serial primary key,
-    name text not null,
-    password text not null,
+    [name] text not null,
+    [password]text not null,
     expert boolean not null,
-    admin boolean not null 
+    [admin] boolean not null 
 );
 
 create table questions (
