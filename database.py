11434c4c78c9003bd63cd0ce8caa547389ad61db
Surfacing600 <74630359+Surfacing600@users.noreply.github.com>
2021-01-31 13:40:47 +0000

    test

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import psycopg2
 from psycopg2.extras import DictCursor
 
 def connect_db():
-    conn = psycopg2.connect('postgres://fhqwgsxsmvqlrs:948a494ef0bace6169dc9f91a02e4ee870ee9e9e9aba7888ba929db1f4bb2a4e@ec2-3-216-181-219.compute-1.amazonaws.com:5432/dea0pfl3pmfih2', cursor_factory=DictCursor)
+    conn = psycopg2.connect('postgres://fmssrfyewkznpu:b8ddbe0a476d2540d3e6fac107e2139dbe54e08464ef611ac3a431f0c1f5a805@ec2-34-235-240-133.compute-1.amazonaws.com:5432/d2fc2u99e5sju4', cursor_factory=DictCursor)
     conn.autocommit = True
     sql = conn.cursor()
     return conn, sql
